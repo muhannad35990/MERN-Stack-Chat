9f672593f730dab0f92b4659d0f9c507bb639f09
muhannadhammada <muhannad.hammada@salvimar.gmbh>
2021-10-07 14:59:15 +0300

    add change chat functionality

--- src/components/SingleChat.tsx
+++ src/components/SingleChat.tsx
@@ -1,24 +1,31 @@
 import { Avatar } from "@mui/material";
 import React from "react";
 interface Iprops {
-  isSelected: boolean;
+  selectedChat: number;
+  id: number;
+  setSelectedChat: any;
 }
-const SingleChat: React.FC<Iprops> = ({ isSelected }) => {
+const SingleChat: React.FC<Iprops> = ({
+  selectedChat,
+  id,
+  setSelectedChat,
+}) => {
   return (
     <div
       className={`h-16 flex items-center border-b-2 border-b-1  justify-between cursor-pointer ${
-        isSelected && "bg-gray-200"
+        id === selectedChat && "bg-gray-200"
       }`}
+      onClick={() => setSelectedChat(id)}
     >
       <div className="flex items-center p-4  py-8 ">
         <Avatar />
         <div className="flex flex-col ml-4 ">
-          <h1 className="font-bold">Muhannad</h1>
-          <h3 className="text-xs">hello, how are you</h3>
+          <h1 className="font-bold text-gray-800">Muhannad</h1>
+          <h3 className="text-xs text-gray-500">hello, how are you</h3>
         </div>
       </div>
 
-      {isSelected && (
+      {id === selectedChat && (
         <div className="w-4 bg-purple-500 h-16 rounded-r-md"></div>
       )}
     </div>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,7 +3,7 @@ import { Settings, Chat } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import SingleChat from "../components/SingleChat";
 const Main = () => {
-  const [selectedChat, setSelectedChat] = useState(null);
+  const [selectedChat, setSelectedChat] = useState(0);
   return (
     <div className="h-90% w-5/6 bg-white absolute top-12 shadow-lg flex rounded-xl">
       <div className="w-1/3 h-full bg-white">
@@ -22,12 +22,32 @@ const Main = () => {
             className="w-90% border-2 rounded-xl  outline-none px-4 text-sm  text-gray-600 py-2 focus:outline-none  focus:ring-1 focus:border-purple-300  transition ease-in duration-100 mb-4"
           />
         </div>
-        <div className="flex flex-col h-full  ">
-          <SingleChat isSelected={false} />
-          <SingleChat isSelected={false} />
-          <SingleChat isSelected={false} />
-          <SingleChat isSelected={true} />
-          <SingleChat isSelected={false} />
+        <div className="flex flex-col h-full">
+          <SingleChat
+            id={1}
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+          />
+          <SingleChat
+            id={2}
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+          />
+          <SingleChat
+            id={3}
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+          />
+          <SingleChat
+            id={4}
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+          />
+          <SingleChat
+            id={5}
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+          />
         </div>
       </div>
       <div className="w-full h-full bg-gray-100"></div>
